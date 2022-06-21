744ead4f8ed90cb2993780a88c1b089df95d0f31
Amer Islamovic <aislamovic01@gmail.com>
2022-06-21 15:40:17 -0400

    Knowledge check 1 upload

---
+++ data_1_checks/KC1.py
@@ -0,0 +1,10 @@
+print('Hello World')
+
+testlist = ['This','is','just','a','test','list']
+print(testlist[2])
+
+testdict = {'star':'trek', 'doctor':'who'}
+print(testdict.get('doctor'))
+
+testtuple = ('Great', 'power','great','responsibility')
+print(testtuple[2])
